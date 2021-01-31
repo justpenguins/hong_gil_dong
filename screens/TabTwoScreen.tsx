8c84e3bdff75829278377f36f71648780cb73bf0
Christopher Hui <chui@coursera.org>
2021-01-30 23:45:53 -0800

    stuff

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,12 +1,44 @@
 import * as React from 'react';
 import {useState, useEffect} from 'react';
 import {Dimensions, StyleSheet} from 'react-native';
-import {Button, Container} from "@material-ui/core";
+import {Button, Modal, TextField} from "@material-ui/core";
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import {Text, View} from '../components/Themed';
 import {LineChart} from 'react-native-chart-kit';
 import {API_KEY} from 'react-native-dotenv';
 import _ from 'lodash';
 import map from 'lodash/map';
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+      textAlign: 'center',
+      alignItems: 'center',
+      justifyContent: "center",
+    },
+  }),
+);
 
 export async function getStockData(stock: string) {
     let response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stock}&interval=5min&apikey=${API_KEY}`);
@@ -47,6 +79,45 @@ export default function Stonks() {
     const [stockName, setStockName] = useState('Loading stock...');
     const [stockDesc, setStockDesc] = useState('');
     const [stockPrice, setStockPrice] = useState('');
+    const [open, setOpen] = React.useState(false);
+    const classes = useStyles();
+    const [modalStyle] = React.useState(getModalStyle);
+    const [amt, setAmt] = useState(0.0);
+    const [price, setPrice] = useState(0.0);
+    const [sell, setSell] = useState(false);
+    const [sellAmt, setSellAmt] = useState(0.0);
+
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    const handleOpenSell = () => {
+      setSell(true);
+    }
+
+    const handleCloseSell = () => {
+      setSell(false);
+    }
+
+    const changeIt = (e: any) => {
+      let temp = e.target.value;
+      if (e.target.value === '') {
+        temp = '0.0';
+      }
+      setAmt(parseFloat(temp));
+    }
+
+    const changeSell = (e: any) => {
+      let temp = e.target.value;
+      if (e.target.value === '') {
+        temp = '0.0';
+      }
+      setSellAmt(parseFloat(temp));
+    }
 
     useEffect(() => {
         const fetchData = async () => {          
@@ -81,6 +152,7 @@ export default function Stonks() {
             setStockName(info['Name']);
             setStockDesc(info['Description']);
             setStockPrice(`Stock price: ${amountData[amountData.length - 1]}`);
+            setPrice(parseFloat(amountData[amountData.length - 1]));
         }
         fetchData();
     }, [])
@@ -91,10 +163,44 @@ export default function Stonks() {
               <Text style={styles.price}>{stockPrice}</Text>
               <Text style={styles.ticker}>{stock}</Text>
               <div style={{display: 'block', position: 'absolute', right: "1.75%", top: "1.75%"}}>
-                <Button style={{color: '#143C1D'}}>Buy</Button>
-                <Button style={{marginLeft: 20, color: '#143c1d'}}>Sell</Button>
+                <Button onClick={handleOpen} style={{color: '#143C1D'}}>Buy</Button>
+                <Button onClick={handleOpenSell} style={{marginLeft: 20, color: '#143c1d'}}>Sell</Button>
               </div>
             </div>
+
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="simple-modal-title"
+              aria-describedby="simple-modal-description">
+                <div style={modalStyle} className={classes.paper}>
+                  <Text style={{fontSize: 25, fontWeight: 'bold'}}>Ok how many u wanna buy tho</Text>
+                  <br/>
+                  <br/>
+                  <TextField required id="standard-required" label="Required" defaultValue="0" onChange={changeIt} />
+                  <br/>
+                  <br/>
+                  <Text style={{fontSize: 18, fontWeight: 'bold'}}>{`It will cost ${Math.round(amt * price)}. u good with that?`}</Text>
+                  <Button>Purchase Amount</Button>
+                </div>
+              </Modal>
+
+              <Modal
+              open={sell}
+              onClose={handleCloseSell}
+              aria-labelledby="simple-modal-title"
+              aria-describedby="simple-modal-description">
+                <div style={modalStyle} className={classes.paper}>
+                  <Text style={{fontSize: 25, fontWeight: 'bold'}}>Ok how many u wanna sell tho</Text>
+                  <br/>
+                  <br/>
+                  <TextField required id="standard-required" label="Required" defaultValue="0" onChange={changeSell} />
+                  <br/>
+                  <br/>
+                  <Text style={{fontSize: 18, fontWeight: 'bold'}}>{`U will get bac ${Math.round(sellAmt * price)}. u good with that?`}</Text>
+                  <Button>Sell Amount</Button>
+                </div>
+              </Modal>
             
             <View style={styles.data}>
 
